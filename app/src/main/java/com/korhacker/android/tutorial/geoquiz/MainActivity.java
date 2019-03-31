294a5a8403a9ef7273476df5f5e304e5abfa2e9c
JungWon Kim <manorgass@gmail.com>
2019-04-01 01:02:55 +0900

    - p. 43 - 필요한 것만 볼까.. ?

--- app/src/main/java/com/korhacker/android/tutorial/geoquiz/MainActivity.java
+++ app/src/main/java/com/korhacker/android/tutorial/geoquiz/MainActivity.java
@@ -2,12 +2,34 @@ package com.korhacker.android.tutorial.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnTrue;
+    private Button btnFalse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTrue = findViewById(R.id.true_button);
+        btnTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnFalse = findViewById(R.id.false_button);
+        btnFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
